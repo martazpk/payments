221277d01f76776b91f5031e24eebc71e6f2e82c
martazpk <kopp.marta@gmail.com>
2020-10-01 10:35:59 +0200

    sink- emits any number of next signals

--- src/main/java/pl/training/payments/PaymentsGenerator.java
+++ src/main/java/pl/training/payments/PaymentsGenerator.java
@@ -2,7 +2,10 @@ package pl.training.payments;
 
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.SynchronousSink;
+import reactor.util.function.Tuple2;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.Random;
 import java.util.UUID;
@@ -12,8 +15,14 @@ public class PaymentsGenerator {
     private static final int MAX_AMOUNT = 10_000;
     private final Random random = new Random();
 
-    public Flux<Payment> paymentStream(){
-        return Flux.just(createPayment(), createPayment(), createPayment());
+    public Flux<Payment> paymentStream(Duration duration){
+        return Flux.generate(this::paymentsSink)
+                .zipWith(Flux.interval(duration))
+                .map(Tuple2::getT1);
+    }
+
+    private void paymentsSink(SynchronousSink<Payment> sink){
+        sink.next(createPayment());
     }
 
     public Payment createPayment(){
--- src/test/java/pl/training/payments/PaymentsGeneratorTest.java
+++ src/test/java/pl/training/payments/PaymentsGeneratorTest.java
@@ -2,15 +2,20 @@ package pl.training.payments;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentsGeneratorTest {
     private PaymentsGenerator paymentsGenerator = new PaymentsGenerator();
     
     @Test
-    void shouldGeneratePayments(){
-        paymentsGenerator.paymentStream()
-                .subscribe(this::onPayment, this::onError);
+    void shouldGeneratePayments() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        paymentsGenerator.paymentStream(Duration.ofSeconds(1))
+                .subscribe(this::onPayment, this::onError, countDownLatch::countDown);
+        countDownLatch.await();
     }
 
     private void onError(Throwable throwable) {

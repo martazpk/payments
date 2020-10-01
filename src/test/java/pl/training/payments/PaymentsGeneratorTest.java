cdec45e39a47f2cc10d798fc3abec8fe3493a9ca
martazpk <kopp.marta@gmail.com>
2020-10-01 11:33:15 +0200

    StepVerifier - a way to test the streams

--- src/test/java/pl/training/payments/PaymentsGeneratorTest.java
+++ src/test/java/pl/training/payments/PaymentsGeneratorTest.java
@@ -1,21 +1,26 @@
 package pl.training.payments;
 
 import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentsGeneratorTest {
     private PaymentsGenerator paymentsGenerator = new PaymentsGenerator();
     
     @Test
-    void shouldGeneratePayments() throws InterruptedException {
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-        paymentsGenerator.paymentStream(Duration.ofSeconds(1))
-                .subscribe(this::onPayment, this::onError, countDownLatch::countDown);
-        countDownLatch.await();
+    void shouldGeneratePayments()  {
+        StepVerifier.create(paymentsGenerator.paymentStream(Duration.ofSeconds(1)).take(2))
+                .recordWith(ArrayList::new)
+                .consumeNextWith(Payment::hasTransactionId)
+                .expectNextCount(1)
+                .consumeRecordedWith(payments -> assertThat(payments).allMatch(Payment::hasTransactionId))
+                .verifyComplete();
     }
 
     private void onError(Throwable throwable) {

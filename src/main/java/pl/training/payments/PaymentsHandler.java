e744002a56ff8c2dc023c50a5f924da1cb25491f
martazpk <kopp.marta@gmail.com>
2020-10-03 18:42:42 +0200

    (Handler + Router) -> like controller test with testWebClient

---
+++ src/main/java/pl/training/payments/PaymentsHandler.java
@@ -0,0 +1,34 @@
+package pl.training.payments;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.awt.*;
+import java.time.Duration;
+
+@Component
+@RequiredArgsConstructor
+public class PaymentsHandler {
+    private static final Duration PAYMENTS_DURATION = Duration.ofSeconds(1);
+    private final PaymentsGenerator paymentsGenerator;
+
+    public Mono<ServerResponse> getPayment(ServerRequest request){
+        return createResponse(paymentsGenerator.paymentStream(PAYMENTS_DURATION).take(1), MediaType.APPLICATION_JSON);
+    }
+
+    public Mono<ServerResponse> getPayments(ServerRequest request){
+        return createResponse(paymentsGenerator.paymentStream(PAYMENTS_DURATION), MediaType.APPLICATION_STREAM_JSON);
+    }
+
+    private Mono<ServerResponse> createResponse(Flux<Payment> paymentStream, MediaType mediaType) {
+        return ServerResponse.ok()
+                .contentType(mediaType)
+                .body(paymentStream, Payment.class);
+    }
+}
--- src/test/java/pl/training/payments/PaymentsApplicationTests.java
+++ src/test/java/pl/training/payments/PaymentsApplicationTests.java
@@ -1,13 +1,43 @@
 package pl.training.payments;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient(timeout = "10000")
 class PaymentsApplicationTests {
 
+    @Autowired
+    private WebTestClient webTestClient;
+
     @Test
-    void contextLoads() {
+    void shouldReturnPayment() {
+        webTestClient.get()
+                .uri("/payment")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Payment.class)
+                .consumeWith(result -> assertThat(result.getResponseBody()).hasSize(1));
     }
 
+    @Test
+    void shouldReturnPayments() {
+        webTestClient.get()
+                .uri("/payments")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_STREAM_JSON)
+                .returnResult(Payment.class)
+                .getResponseBody().take(2)
+                .subscribe(payment -> assertTrue(payment.hasTransactionId()));
+    }
 }
